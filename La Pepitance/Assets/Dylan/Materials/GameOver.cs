e99c11050852f9215c4406fad532b8b73248b6cb
Dylan Santos de Pinho <dylan.santosdepinho@gmail.com>
2018-05-03 22:08:18 +0200

    GameOver Scene

---
+++ La Pepitance/Assets/Dylan/Materials/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start ()
+	{
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}

b271a0b842f03789320d8df09ee1b38e96ade3d9
Luc Gigon <luc.gigon@he-arc.ch>
2018-05-05 19:02:31 +0200

    modification menu (background, lumiere, lumiere clignotante, responsive)

---
+++ La Pepitance/Assets/Luc/Scripts/FlashingLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour {
+
+	public Light lightFlashing;
+	public float minWaitTime;
+	public float maxWaitTime;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Flashing ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Flashing(){
+		while (true) {
+			yield return new WaitForSeconds (Random.Range (minWaitTime, maxWaitTime));
+			lightFlashing.enabled = !lightFlashing.enabled;
+		}
+	}
+}
